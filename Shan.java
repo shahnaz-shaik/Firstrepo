fa108b2d5b8851e044d8ea7a3ca51baf4ce4ad15
shahnaz-shaik <153435451+shahnaz-shaik@users.noreply.github.com>
2023-12-15 10:49:07 +0530

    Create Shan.java

---
+++ Shan.java
@@ -0,0 +1,31 @@
+public class Shan {
+	public static void main(String[] args) {
+		int a[][]= {{1,32,3},{4,5,6},{3,8,549}};
+		int b[][]= {{9,8,7},{32,556,4},{32,2,1}};
+		int exist=0;
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a[i].length;j++) {
+				exist=0;
+				for(int x=0;x<b.length;x++) {
+					
+					for(int y=0;y<b[x].length;y++) {
+						if(a[i][j]==b[x][y]) {
+							System.out.println(a[i][j]);
+							exist=1;
+							
+						}
+						if(exist==1) {
+							break;
+						}
+						
+					}
+					
+					
+
+				}
+
+			}
+		}
+	}
+
+}
